9760e4619a2f1a4e2cebfb06d2c6b9a112823a63
Sebastian <sebastian.r.rb@gmail.com>
2017-07-29 22:01:55 +0100

    Add picture of Jake to gallery

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,9 @@
 jQuery(document).ready(function($) {
-
-    var $modal = $('.image-modal');
-    var $imageGrid = $('.section--about-me__image-grid');
+    var $modal = $(".image-modal");
+    var $imageGrid = $(".section--about-me__image-grid");
     var $imageGridContainers = $(".section--about-me__image-grid-container");
-    var $modalImageContainer = $('.image-modal__modal-image');
-    var $modalText = $('.image-modal__modal-text-content');
+    var $modalImageContainer = $(".image-modal__modal-image");
+    var $modalText = $(".image-modal__modal-text-content");
     var $modalCloseButton = $(".image-modal__close-modal");
     var gridImages = Array.from(document.querySelectorAll(".section--about-me__image-grid-container"));
     var mainElement = document.getElementsByTagName("main")[0];
@@ -15,13 +14,12 @@ jQuery(document).ready(function($) {
     var $imageClone;
     var $matchedGridImage;
 
-
     // console.log(rightNavArrow);
 
     var modalOpenTimeline = new TimelineMax();
     var modalCloseTimeline = new TimelineMax();
 
-    $imageGrid.on('click', ".section--about-me__image-grid-container", function(event) {
+    $imageGrid.on("click", ".section--about-me__image-grid-container", function(event) {
         event.preventDefault();
         var $clickedImage = $(event.currentTarget);
         // console.log($clickedImage);
@@ -33,7 +31,7 @@ jQuery(document).ready(function($) {
 
         //get clone of inner image
         $imageClone = $clickedImage.find("img").clone();
-        $imageClone.addClass('image-clone');
+        $imageClone.addClass("image-clone");
         // console.log($imageClone);
 
         //populate description
@@ -54,27 +52,25 @@ jQuery(document).ready(function($) {
             zIndex: 120
         });
 
-        $('main').append($imageClone);
+        $("main").append($imageClone);
 
-        $imageClone[0].style.display = 'none';
+        $imageClone[0].style.display = "none";
         setTimeout(function() {
-            $imageClone[0].style.display = 'block';
+            $imageClone[0].style.display = "block";
         }, 10);
 
         showModal();
-
     });
 
-
-    $modalCloseButton.on('click', function(event) {
+    $modalCloseButton.on("click", function(event) {
         event.preventDefault();
 
         //get imageClone href
-        var modalImageHREF = $imageClone.attr('src');
+        var modalImageHREF = $imageClone.attr("src");
 
         //find image in grid with same href
         $imageGridContainers.each(function(index, element) {
-            if($(element).find("img").attr("src") === modalImageHREF) {
+            if ($(element).find("img").attr("src") === modalImageHREF) {
                 $matchedGridImage = $(element);
             }
         });
@@ -86,46 +82,59 @@ jQuery(document).ready(function($) {
 
     //show modal
     function showModal() {
-
         var modalImageMeasurements = $modalImageContainer[0].getBoundingClientRect();
         // console.log(modalImageMeasurements);
 
-        modalOpenTimeline.to($modal, .6, {
+        modalOpenTimeline.to($modal, 0.6, {
             className: "+=image-modal--shown"
         });
 
         modalOpenTimeline.add(function() {
-            $(".header__nav-bar-container").addClass('faded-for-modal');
+            $(".header__nav-bar-container").addClass("faded-for-modal");
         }, "-=.5");
 
-
         //move image clone over modal image container
-        modalOpenTimeline.to($imageClone, .9, {
-            // ease: Power1.easeOut,
-            ease: Power1.easeInOut,
-            top: modalImageMeasurements.top + 20,
-            left: modalImageMeasurements.left,
-            width: modalImageMeasurements.width,
-            height: modalImageMeasurements.height
-            // padding: "5px",
-            // background: "#f94c0c"
-            // background: "rgb(167, 68, 31)"
-        }, "-=.2");
+        modalOpenTimeline.to(
+            $imageClone,
+            0.9,
+            {
+                // ease: Power1.easeOut,
+                ease: Power1.easeInOut,
+                top: modalImageMeasurements.top + 20,
+                left: modalImageMeasurements.left,
+                width: modalImageMeasurements.width,
+                height: modalImageMeasurements.height
+                // padding: "5px",
+                // background: "#f94c0c"
+                // background: "rgb(167, 68, 31)"
+            },
+            "-=.2"
+        );
 
         //animate in description text
-        modalOpenTimeline.to($modalText, .7, {
+        modalOpenTimeline.to($modalText, 0.7, {
             className: "+=image-modal__modal-text-content--shown"
         });
 
         //animate in description text
-        modalOpenTimeline.to($modalCloseButton, .5, {
-            pointerEvents: "none",
-            className: "+=image-modal__close-modal--shown"
-        }, "-=.3");
+        modalOpenTimeline.to(
+            $modalCloseButton,
+            0.5,
+            {
+                pointerEvents: "none",
+                className: "+=image-modal__close-modal--shown"
+            },
+            "-=.3"
+        );
 
-        modalOpenTimeline.to($modalCloseButton, .1, {
-            pointerEvents: "auto"
-        }, "+=.5");
+        modalOpenTimeline.to(
+            $modalCloseButton,
+            0.1,
+            {
+                pointerEvents: "auto"
+            },
+            "+=.5"
+        );
 
         modalOpenTimeline.add(function() {
             body.style.overflowY = "hidden";
@@ -133,52 +142,63 @@ jQuery(document).ready(function($) {
     }
 
     function hideModal() {
-
         //matched grid image measurements
         var matchedGridImageMeasurements = $matchedGridImage[0].getBoundingClientRect();
         // console.log(matchedGridImageMeasurements);
 
-       // // scroll to position of current image on grid
-       window.scrollTo(0, (matchedGridImageMeasurements.top + window.pageYOffset - 250));
+        // // scroll to position of current image on grid
+        window.scrollTo(0, matchedGridImageMeasurements.top + window.pageYOffset - 250);
 
-       matchedGridImageMeasurements = $matchedGridImage[0].getBoundingClientRect();
+        matchedGridImageMeasurements = $matchedGridImage[0].getBoundingClientRect();
 
-       modalCloseTimeline.add(function() {
+        modalCloseTimeline.add(function() {
             body.style.pointerEvents = "none";
-       });
+        });
 
         //animate out description text
-        modalCloseTimeline.to($modalText, .7, {
+        modalCloseTimeline.to($modalText, 0.7, {
             className: "-=image-modal__modal-text-content--shown"
         });
 
         //animate in description text
-        modalCloseTimeline.to($modalCloseButton, .7, {
-            className: "-=image-modal__close-modal--shown"
-        }, "-=.7");
-
-
+        modalCloseTimeline.to(
+            $modalCloseButton,
+            0.7,
+            {
+                className: "-=image-modal__close-modal--shown"
+            },
+            "-=.7"
+        );
 
         //move image clone over grid image
-        modalCloseTimeline.to($imageClone, .5, {
-            ease: Power1.easeInOut,
-            top: matchedGridImageMeasurements.top,
-            left: matchedGridImageMeasurements.left,
-            width: matchedGridImageMeasurements.width,
-            height: matchedGridImageMeasurements.height,
-            boxShadow: "0 0 0 0 transparent"
-            // padding: "0"
-        }, "-=.2");
-
+        modalCloseTimeline.to(
+            $imageClone,
+            0.5,
+            {
+                ease: Power1.easeInOut,
+                top: matchedGridImageMeasurements.top,
+                left: matchedGridImageMeasurements.left,
+                width: matchedGridImageMeasurements.width,
+                height: matchedGridImageMeasurements.height,
+                boxShadow: "0 0 0 0 transparent"
+                // padding: "0"
+            },
+            "-=.2"
+        );
 
         modalCloseTimeline.add(function() {
-            $(".header__nav-bar-container").removeClass('faded-for-modal');
+            $(".header__nav-bar-container").removeClass("faded-for-modal");
         }, "-=.1");
 
         //hide modal
-        modalCloseTimeline.to($modal, .7, {
-            className: "-=image-modal--shown"
-        }, "+=0");
+        modalCloseTimeline.to(
+            $modal,
+            0.7,
+            {
+                className: "-=image-modal--shown"
+            },
+            "+=0"
+        );
 
         modalCloseTimeline.add(function() {
             $imageClone.remove();
@@ -189,7 +209,6 @@ jQuery(document).ready(function($) {
             body.style.overflowY = "scroll";
         }, "+=.3");
     }
-
 
     rightNavArrow.addEventListener("click", nextImage);
     leftNavArrow.addEventListener("click", previousImage);
@@ -209,33 +228,34 @@ jQuery(document).ready(function($) {
         //get dimensions of lastImage
         // var lastIMageMeasurements = oldImage.getBoundingClientRect();
 
-
         //style last image clone with high z-index
         lastImageClone.style.zIndex = "150";
 
         //append old image clone
         mainElement.appendChild(lastImageClone);
 
-        oldImage.style.display = 'none';
+        oldImage.style.display = "none";
         setTimeout(function() {
-            oldImage.style.display = 'block';
+            oldImage.style.display = "block";
         }, 0.5);
 
         //increment selected image ID by one; if at end, go back to beginning
-        if(selectedImageID === gridImages.length - 1) {
+        if (selectedImageID === gridImages.length - 1) {
             selectedImageID = 0;
         } else {
             selectedImageID++;
         }
 
         //get src of image with that ID
-        var nextImageSrc = gridImages[selectedImageID].querySelector(".section--about-me__image-grid-image").getAttribute("src");
+        var nextImageSrc = gridImages[selectedImageID]
+            .querySelector(".section--about-me__image-grid-image")
+            .getAttribute("src");
 
         //create image clone with old
         oldImage.setAttribute("src", nextImageSrc);
 
         // transition out old image clone
-        setTimeout(function(){
+        setTimeout(function() {
             lastImageClone.classList.add("slide-out");
             $modalText.text(imageDescriptions[selectedImageID + 1]);
         }, 200);
@@ -270,15 +290,16 @@ jQuery(document).ready(function($) {
         // mainElement.appendChild(lastImageClone);
 
         //increment selected image ID by one; if at end, go back to beginning
-        if(selectedImageID === 0) {
+        if (selectedImageID === 0) {
             selectedImageID = gridImages.length - 1;
         } else {
             selectedImageID--;
         }
 
         //get src of image with that ID
-        var nextImageSrc = gridImages[selectedImageID].querySelector(".section--about-me__image-grid-image").getAttribute("src");
-
+        var nextImageSrc = gridImages[selectedImageID]
+            .querySelector(".section--about-me__image-grid-image")
+            .getAttribute("src");
 
         lastImageClone.setAttribute("src", nextImageSrc);
 
@@ -289,17 +310,16 @@ jQuery(document).ready(function($) {
         // oldImage.setAttribute("src", nextImageSrc);
 
         //transition out old image clone
-        setTimeout(function(){
+        setTimeout(function() {
             // lastImageClone.classList.add("slide-out");
             lastImageClone.classList.remove("to-slide-in");
             $modalText.text(imageDescriptions[selectedImageID + 1]);
         }, 200);
 
-
-        setTimeout(function(){
-            oldImage.style.display = 'none';
+        setTimeout(function() {
+            oldImage.style.display = "none";
             setTimeout(function() {
-                oldImage.style.display = 'block';
+                oldImage.style.display = "block";
             }, 0.5);
             lastImageClone.style.boxShadow = "0 0 0 0 transparent";
         }, 1100);
@@ -311,7 +331,6 @@ jQuery(document).ready(function($) {
             rightNavArrow.style.pointerEvents = "auto";
         }, 1500);
     }
-
 
     var imageDescriptions = {
         1: "My fiancée and me on a hike in Spain",
@@ -326,8 +345,8 @@ jQuery(document).ready(function($) {
         10: "My fiancée and me in San Francisco",
         11: "My brother and me on vacation in Spain",
         12: "Me and my brother at a wedding",
-       13: "Me in Spain",
-        14: "My friends, fiancée, and me on a hike in Upstate New York",
+        13: "Me in Spain",
+        14: "My dad, Jake, giving a talk at a conference on environmental preservation",
         15: "My friends and me on a ski trip in Lake Tahoe, California"
     };
 });
